251e6aefcff4a768615f19dfb150221395c001a3
milad <miladyousefi4s@gmail.com>
2022-06-16 15:10:48 +0430

    add pageinator to book_list.html

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # authentication settings
 AUTH_USER_MODEL = 'accounts.CustomUser'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'book_list'
+LOGOUT_REDIRECT_URL = 'book_list'
 
 # crispy forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
