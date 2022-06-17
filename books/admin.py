2828a758a93de9c42383e33b54997bb0b1bd4084
milad <miladyousefi4s@gmail.com>
2022-06-18 00:20:55 +0430

    add comment to admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
 
-admin.site.register(Book)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['book', 'user','is_active', 'recommend', 'datetime_created']
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'price']
+
+
+admin.site.register(Book, BookAdmin)
